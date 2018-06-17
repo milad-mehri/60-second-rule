8344e6d2ec6ca6ab43f78e5ec93aed466f6cc260
Milad Mehri <memm.milad@gmail.com>
2018-06-17 15:24:53 -0700

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name 60 Second Rule
+// @version 1.0
+// @description  Spams the submit button every 1 seconds until the forum post submits
+// @match *://scratch.mit.edu/*
+// ==/UserScript==
+if(document.getElementsByClassName("errorlist").length == 1){
+  if(document.getElementsByClassName("errorlist")[0].children[0].innerHTML == "Sorry, you have to wait 60 seconds between posts."){
+    window.n = window.setInterval(function(){document.getElementsByName("AddPostForm")[0].click();}, 1000);
+    var g = document.createElement("BUTTON");
+    g.innerHTML = "Cancel Userscript";
+    g.addEventListener("click", function(){
+      g.width = 0;
+      g.height = 0;
+      window.clearInterval(window.n);
+
+    });
+    document.getElementsByClassName("errorlist")[0].children[0].appendChild(g);
+  }
+}
